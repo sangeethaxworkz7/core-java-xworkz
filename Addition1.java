6ce8aff0f42c69da3b8036c46ae6e6806f59a57f
punith punith <sangeethaxworkx7@gmail.com>
2023-08-22 11:15:20 +0530

    message

---
+++ Addition1.java
@@ -0,0 +1,36 @@
+class Addition1{
+
+    public static void main(String addition[]){
+	Addition1.add (50, 60, 122);
+	add ( 55, 65, 72);
+	add ( 595, 66, 725);
+	add ( 585, 67);
+	add ( 575, 656, 722);
+	add ( 56, 657);
+	add ( 555, 651, 724);
+	add ( 545, 652, 726);
+	add ( 57, 653);
+	add ( 525, 654, 728);
+	add ( 515, 655, 782);
+	add ( 50, 666);
+	add ( 59, 625, 872);
+	add ( 558, 615);
+	add ( 557, 635, 672);
+	add ( 556, 645, 572);
+	add ( 555, 645, 472);
+	add ( 54, 659);
+	add ( 553, 245, 272);
+    add ( 552, 465, 722);
+	add ( 51, 565);
+	}
+	
+	public static void add(int a, int b)
+	{
+	System.out.println(a+b);
+	}
+	
+	public static void add(int a, int b, int c){
+	System.out.println(a+b+c);
+	}
+	
+	}
